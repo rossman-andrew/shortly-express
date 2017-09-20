5429e89dce3480c5ea95667c971ac94e98043ed6
rossman-andrew <andrew100r@gmail.com>
2017-09-20 16:51:02 -0700

    implement login

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,9 @@
 const parseCookies = (req, res, next) => {
+  // access cookie from request
+  // if valid, parse them into an object
+  // assign to cookies property on the request
+  console.log('trying to find cookie', req, req.body.cookies);
+
 };
 
 module.exports = parseCookies;
